391a42fc9355969d1990e8eb4bb2842339006753
Roncelli <leonardoroncelli@gmail.com>
2024-12-30 17:30:38 +0100

    Attributi, costruttore e metodi set e get.

--- src/OperaDarte.java
+++ src/OperaDarte.java
@@ -1,3 +1,20 @@
 public class OperaDarte {
-
+    private String artista;
+    private String titolo;
+    public OperaDarte(String artista,String titolo){
+        this.artista=artista;
+        this.titolo=titolo;
+    }
+    public void setArtista(String artista){
+        this.artista=artista;
+    }
+    public String getArtista(){
+        return artista;
+    }
+    public void setTitolo(String titolo){
+        this.titolo=titolo;
+    }
+    public String getTitolo(){
+        return titolo;
+    }
 }
